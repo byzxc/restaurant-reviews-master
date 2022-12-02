ad506e0fc9bb94a00ba2ae044291882909700440
byzxc <darrenlinby@hotmail.com>
2022-12-02 22:59:59 +0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
+/* import logo from './logo.svg';
+import './App.css'; */
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import {Route, Link, Routes, BrowserRouter} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import AddReview from "./components/add-review";
 import Restaurant from "./components/restaurants";
@@ -16,59 +18,41 @@ function App() {
 
   async function logout() {
     setUser(null)
-  }
-
+  } 
+  
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
-          Restaurant Reviews
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/restaurants"} className="nav-link">
-              Restaurants
-            </Link>
-          </li>
-          <li className="nav-item" >
-            { user ? (
-              <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
-                Logout {user.name}
-              </a>
-            ) : (            
-            <Link to={"/login"} className="nav-link">
-              Login
-            </Link>
-            )}
+    <BrowserRouter>
+      <div className="App">
+        
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/restaurants" className="navbar-brand">
+            Restaurant Reviews
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/restaurants"} className="nav-link">Restaurants</Link>
+            </li>
+            <li className="nav-item" >
+              { user 
+                ? (<a href="/" onClick={logout} className="nav-link" style={{cursor:'pointer'}}>Logout {user.name}</a>) 
+                : (<Link to={"/login"} className="nav-link">Login</Link>)
+              }
 
-          </li>
+            </li>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<RestaurantsList/>} />
+            <Route path="/restaurants/*" element={<RestaurantsList/>} />
+            <Route path="/restaurants/:id/review" element={<AddReview />}/>
+            <Route path="/restaurants/:id" element={<Restaurant />}/>
+            <Route path="/login" element={<Login login={login} />}/>
+          </Routes>
         </div>
-      </nav>
-
-      <div className="container mt-3">
-        <Switch>
-          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
-          <Route 
-            path="/restaurants/:id/review"
-            render={(props) => (
-              <AddReview {...props} user={user} />
-            )}
-          />
-          <Route 
-            path="/restaurants/:id"
-            render={(props) => (
-              <Restaurant {...props} user={user} />
-            )}
-          />
-          <Route 
-            path="/login"
-            render={(props) => (
-              <Login {...props} login={login} />
-            )}
-          />
-        </Switch>
       </div>
-    </div>
+    </BrowserRouter>
+
   );
 }
 
